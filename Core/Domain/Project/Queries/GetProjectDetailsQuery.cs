a03cf4b43ee77e653884c2d37c04a0276868b410
Berkay Yerdelen <berkayyerdelen@gmail.com>
2020-03-05 16:20:16 +0300

    mapping

--- Core/Domain/Project/Queries/GetProjectDetailsQuery.cs
+++ Core/Domain/Project/Queries/GetProjectDetailsQuery.cs
@@ -10,9 +10,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Core.Domain.Project.Queries
 {
-    public class GetProjectDetailsQuery:IRequest<List<global::Domain.Entities.Project>>
+    public class GetProjectDetailsQuery:IRequest<List<GetProjectDetailDto>>
     {
-        public class Handler : IRequestHandler<GetProjectDetailsQuery, List<global::Domain.Entities.Project>>
+        public class Handler : IRequestHandler<GetProjectDetailsQuery, List<GetProjectDetailDto>>
         {
             public readonly IApplicationDbContext _context;
             public readonly IMapper _mapper;
@@ -22,12 +22,13 @@ namespace Core.Domain.Project.Queries
                 _mapper = mapper;
             }
 
-            public async Task<List<global::Domain.Entities.Project>> Handle(GetProjectDetailsQuery request, CancellationToken cancellationToken)
+            public async Task<List<GetProjectDetailDto>> Handle(GetProjectDetailsQuery request, CancellationToken cancellationToken)
             {
 
                 var projectDetails = await _context.Set<global::Domain.Entities.Project>().
-                Include(x => x.Webapps).ThenInclude(c=>c.WebAppDetails).
-                ToListAsync(cancellationToken);               
+                Include(x => x.Webapps).ThenInclude(c=>c.WebAppDetails)
+                .Select(x=>_mapper.Map<GetProjectDetailDto>(x))
+                .ToListAsync(cancellationToken);               
                 return projectDetails;
                        
                

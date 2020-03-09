d139111286d1e98cdb597a2103d2a5c79213b3cd
Berkay Yerdelen <berkayyerdelen@gmail.com>
2020-03-09 09:29:05 +0300

    restclient dto

--- Core/Domain/Project/Queries/RestApi/RestApiQueryHandler.cs
+++ Core/Domain/Project/Queries/RestApi/RestApiQueryHandler.cs
@@ -1,5 +1,7 @@
 ﻿using Core.Domain.Project.Queries.RestApi.Dto;
+using Domain.Enums;
 using MediatR;
+using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,14 +10,32 @@ using System.Threading.Tasks;
 
 namespace Core.Domain.Project.Queries.RestApi
 {
-    public class RestApiQueryHandler : IRequest<RestApiRequestDto>
+    public class RestApiQueryHandler : IRequest<RestApiResponseDto>
     {
-        //public class Handler : IRequestHandler<RestApiQueryHandler, RestApiResponseDto>
-        //{
-        //    public Task<RestApiResponseDto> Handle(RestApiQueryHandler request, CancellationToken cancellationToken)
-        //    {
-        //        throw new NotImplementedException();
-        //    }
-        //}
+        public string ApiURL { get; set; }
+        public string Token { get; set; }
+        public HttpType HttpType { get; set; }
+        public string Request { get; set; }
+        public class Handler : IRequestHandler<RestApiQueryHandler, RestApiResponseDto>
+        {
+
+            public Task<RestApiResponseDto> Handle(RestApiQueryHandler request, CancellationToken cancellationToken)
+            {
+                var client = new RestClient(request.ApiURL);
+                switch (request.HttpType)
+                {
+                    case HttpType.Get:
+                        
+                    case HttpType.Post:
+                        
+                    case HttpType.Delete:
+                        
+                    case HttpType.Put:
+                        
+                    default:
+                        break;
+                }
+            }
+        }
     }
 }
